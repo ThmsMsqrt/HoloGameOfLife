0ecab5f14bea662fd257e1cb7b10633f90a3a742
Thomas Masquart <thomas.masquart@absolute.de>
2017-05-17 14:55:11 +0200

    Create the unity project, add the basics library and the basics project folders.

---
+++ HoloGameOfLife/Assets/HoloToolkitExtensions/Scripts/Messaging/MessengerCallback.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HoloToolkitExtensions.Messaging
+{
+    public delegate void MessengerCallback<in T>(T arg);
+}
---
+++ HoloGameOfLife/Assets/HoloToolkitExtensions/Scripts/Utilites/LookingDirectionHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.SpatialMapping;
+using UnityEngine;
+
+namespace HoloToolkitExtensions.Utilies
+{
+    /// <summary>
+    /// Help to calculate the user's head position
+    /// </summary>
+    public class LookingDirectionHelpers
+    {
+        // will be reused in different projects
+        public static Vector3 GetPositionInLookingDirection(float maxDisance = 2.0f, BaseRayStabilizer stabilizer = null)
+        {
+            RaycastHit hitInfo;
+
+            var ray = stabilizer != null ? stabilizer.StableRay : new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            // TODO: check if it solves the problems for the drones project.
+            if (SpatialMappingManager.Instance != null && Physics.Raycast(ray, out hitInfo, maxDisance, SpatialMappingManager.Instance.LayerMask))
+            {
+                return hitInfo.point;
+            }
+            return CalculatePositionDeadAhead(maxDisance);
+        }
+
+        public static Vector3 CalculatePositionDeadAhead(float distance = 2.0f, BaseRayStabilizer stabilizer = null)
+        {
+            return stabilizer != null
+                ? stabilizer.StableRay.origin + stabilizer.StableRay.direction.normalized * distance
+                : Camera.main.transform.position + Camera.main.transform.forward.normalized * distance;
+
+        }
+    }
+}
